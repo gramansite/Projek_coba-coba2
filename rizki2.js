36138725cb876de47eb03e30e1d8e05998bf7807
gramansite <gramansite@gmail.com>
2021-03-23 17:50:40 +0700

    update all

---
+++ rizki2.js
@@ -0,0 +1,15 @@
+$(function () {
+  var pull = $("#pull"); // Variabel tombol navigasi (akan muncul hanya pada perangkat mobile)
+  menu = $("nav ul"); // Variabel menu
+
+  $(pull).on("click", function (e) {
+    e.preventDefault();
+    menu.slideToggle();
+  });
+  $(window).resize(function () {
+    var w = $(window).width();
+    if (w > 600 && menu.is(":hidden")) {
+      menu.removeAttr("style");
+    }
+  });
+});
